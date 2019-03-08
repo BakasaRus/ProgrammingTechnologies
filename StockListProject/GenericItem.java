453788295f61199b3c9727398fe0b7032490caf7
Nick Andreev <bakasaru@list.ru>
2019-03-08 21:06:36 +0300

    Start implementing lab 2, exercise 1-1

---
+++ StockListProject/GenericItem.java
@@ -0,0 +1,11 @@
+package StockListProject;
+
+public class GenericItem {
+    public int ID;
+	public String name;
+	public float price;
+
+	void printAll(){
+        System.out.printf("ID: %d , Name: %-20s , price:%5.2f \n",ID,name,price);
+    }
+}
---
+++ StockListProject/Main.java
@@ -0,0 +1,25 @@
+package StockListProject;
+
+public class Main {
+    public static void main(String[] args) {
+        GenericItem item1 = new GenericItem();
+        GenericItem item2 = new GenericItem();
+        GenericItem item3 = new GenericItem();
+
+        item1.ID = 1;
+        item1.name = "Рафаэлло";
+        item1.price = 249.90f;
+
+        item2.ID = 2;
+        item2.name = "Милка";
+        item2.price = 119.90f;
+
+        item3.ID = 3;
+        item3.name = "Риттер Спорт";
+        item3.price = 99.90f;
+
+        item1.printAll();
+        item2.printAll();
+        item3.printAll();
+    }
+}
