90e84d83d5340e7825f09c7a1d9b9b0f9d6a2325
Nick Andreev <bakasaru@list.ru>
2019-04-30 00:03:35 +0300

    Add Bank class for lab 6-2

---
+++ Sync/U1901Bank.java
@@ -0,0 +1,26 @@
+package Sync;
+
+public class U1901Bank {
+    private int to = 0;
+    private int from = 220;
+
+    public void calc(int transaction, long timeout) {
+        System.out.println(
+            "Before: Thread = " + Thread.currentThread().getName() +
+            ", To = " + to +
+            ", From = " + from
+        );
+        from -= transaction;
+        try {
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        to += transaction;
+        System.out.println(
+            "After: Thread = " + Thread.currentThread().getName() +
+            ", To = " + to +
+            ", From = " + from
+        );
+    }
+}
